67a867c8f71ec1619cb721def5ee041c9509d9e5
Daria <daria.diachkova@mail.ru>
2024-02-21 18:40:03 +0300

    add links

--- src/pages/404Page.tsx
+++ src/pages/404Page.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const NoMatch = () => {
   return (
     <div>
-      <h2 className="caveat-700">Такой страницы нет!</h2>
+      <h2>Такой страницы нет!</h2>
       <p>
         <Link to="/">Вернуться на главную</Link>
       </p>
--- src/pages/ReadPage.tsx
+++ src/pages/ReadPage.tsx
@@ -2,6 +2,21 @@ export const Read = () => {
   return (
     <div>
       <h2 className="lead-in">Статьи</h2>
+      <ul className="articles_list">
+        {ARTICLES.map(article =>
+          <li key={article.title}>
+            <a href={`${article.link}`} target="_blank">{article.title}</a>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
+
+const ARTICLES = [
+  { title: 'Название статьи есть', link: '/some-link' },
+  { title: 'Название статьи 2', link: '/some-link' },
+  { title: 'Название статьи 3', link: '/some-link' },
+  { title: 'Название статьи 4', link: '/some-link' },
+  { title: 'Название статьи тут', link: '/some-link' },
+]
