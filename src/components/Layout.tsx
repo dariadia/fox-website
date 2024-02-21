0de289fb1d33d4d9ab2bcacfe80a8684565cb7f8
Daria <daria.diachkova@mail.ru>
2024-02-21 14:14:20 +0300

    extend layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,26 +2,30 @@ import { Link, Outlet } from "react-router-dom"
 
 export const Layout = () => {
   return (
-    <main className="section_main">
-      <h1>Название сайта</h1>
-      <p>
-        Какой-то вводный текст.
-      </p>
-      <nav className="navigation_top">
-        <ul>
-          <li>
-            <Link to="/">Главная</Link>
-          </li>
-          <li>
-            <Link to="/about">Обо мне</Link>
-          </li>
-          <li>
-            <Link to="/prices">Услуги и цены</Link>
-          </li>
-        </ul>
-      </nav>
-      <hr />
-      <Outlet />
-    </main>
+    <>
+      <header>
+        <h1>Название сайта</h1>
+        <p>
+          Какой-то вводный текст.
+        </p>
+        <nav className="navigation_top">
+          <ul>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+            <li>
+              <Link to="/about">Обо мне</Link>
+            </li>
+            <li>
+              <Link to="/prices">Услуги и цены</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
   )
 }
