a94f9ecac1d18ba83162758927f4ed631b4376ca
Daria <daria.diachkova@mail.ru>
2024-02-21 16:58:52 +0300

    style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,22 @@
 import { Link, Outlet } from "react-router-dom"
 
 export const Layout = () => {
-  return (
-    <>
-      <header>
-        <h1>Название сайта</h1>
-        <p>
-          Какой-то вводный текст.
-        </p>
-        <nav className="navigation_top">
+  return (<>
+    <header>
+      <h1>Название сайта</h1>
+      <p>
+        Какой-то вводный текст.
+      </p>
+      <div className="nav">
+        <input type="checkbox" id="nav-check" />
+        <div className="nav-btn">
+          <label for="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+        <nav className="navigation_top nav-links">
           <ul>
             <li>
               <Link to="/">Главная</Link>
@@ -19,13 +27,13 @@ export const Layout = () => {
             <li>
               <Link to="/prices">Услуги и цены</Link>
             </li>
-          </ul>
-        </nav>
-        <hr />
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
+          </ul></nav>
+      </div>
+      <hr />
+    </header>
+    <main>
+      <Outlet />
+    </main>
+  </>
   )
 }
