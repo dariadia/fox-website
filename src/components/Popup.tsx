70b9bc2029c65e429ee1309a57317b41c6f4caa1
Daria <daria.diachkova@mail.ru>
2024-02-22 18:25:26 +0300

    add new data

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -3,7 +3,7 @@ export const Popup = () => <div className="popup_wrapper">
   <p>Оплатив покупку, вы получите постоянный доступ ко всем статьям сайта, отмеченным "&#128274;"</p>
   <h4>Купить сейчас!</h4>
   <form method="POST" action="https://yoomoney.ru/quickpay/confirm">
-    <input type="hidden" name="receiver" value="41001xxxxxxxxxxxx"/>
+    <input type="hidden" name="receiver" value={process.env.REACT_APP_WALLET_NUMBER}/>
     <input type="hidden" name="label" value="застатьи"/>
     <input type="hidden" name="quickpay-form" value="button"/>
     <input type="hidden" name="sum" value="2000" data-type="number"/>
