ede0b3756d7a430728f4c3ef4c97a00c00a8261f
Daria <daria.diachkova@mail.ru>
2024-02-22 18:30:21 +0300

    add mobile styles

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -3,12 +3,12 @@ export const Popup = () => <div className="popup_wrapper">
   <p>Оплатив покупку, вы получите постоянный доступ ко всем статьям сайта, отмеченным "&#128274;"</p>
   <h4>Купить сейчас!</h4>
   <form method="POST" action="https://yoomoney.ru/quickpay/confirm">
-    <input type="hidden" name="receiver" value={process.env.REACT_APP_WALLET_NUMBER}/>
+    <input type="hidden" name="receiver" value="4100151611075##" />
     <input type="hidden" name="label" value="застатьи"/>
     <input type="hidden" name="quickpay-form" value="button"/>
     <input type="hidden" name="sum" value="2000" data-type="number"/>
     <label className="payment_label"><input type="radio" name="paymentType" value="PC"/>ЮMoney</label>
-    <label className="payment_label"><input checked type="radio" name="paymentType" value="AC"/>Банковской картой</label>
+    <label className="payment_label"><input defaultChecked type="radio" name="paymentType" value="AC"/>Банковской картой</label>
     <input className="payment_button" type="submit" value="Оплатить"/>
 </form>
 </div>
